f21cb14d2fa68ec2355fdc75179e05485cc56f7b
Mingxun Wang <mwang87@gmail.com>
2019-11-25 08:57:28 -0800

    updating the webserver to support parallel processing on the worker side

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source activate rdkit
-#gunicorn -w 16 -b 0.0.0.0:5000 --timeout 3600 --keep-alive 3600 main:app --access-logfile /app/logs/access.log
-python ./main.py
+gunicorn -w 8 -b 0.0.0.0:5000 --timeout 3600 --keep-alive 3600 main:app --access-logfile /app/logs/access.log
+#python ./main.py
