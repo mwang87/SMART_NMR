637b7112dbbaa17effe490cd05a733c5f238c134
Mingxun Wang <mwang87@gmail.com>
2019-12-04 13:44:26 -0800

    adding test server

--- test/test_server.py
+++ test/test_server.py
@@ -1,6 +1,11 @@
 import requests
 
-def test_entry():
+def test_entry_test():
     url = "https://mingwangbeta.ucsd.edu/analyzeentryclassic"
+    PARAMS = {'peaks': open("CDCl3_SwinholideA.csv").read()} 
+    requests.post(url, params=PARAMS)
+
+def test_entry_dev():
+    url = "https://smart-dev.ucsd.edu/analyzeentryclassic"
     PARAMS = {'peaks': open("CDCl3_SwinholideA.csv").read()} 
     requests.post(url, params=PARAMS)
