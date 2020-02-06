93903317a1e8392c43e5cbb246a673c9dc377536
Mingxun Wang <mwang87@gmail.com>
2020-02-06 14:21:10 -0800

    adding integration tests

---
+++ test-integration/test-integration.py
@@ -0,0 +1,31 @@
+import requests
+import os
+import pandas as pd
+import json
+
+PRODUCTION_URL = os.environ.get("SERVER_URL", "https://smart.ucsd.edu")
+
+def test_heartbeat():
+    r = requests.get(f"{PRODUCTION_URL}/heartbeat")
+    r.raise_for_status()
+
+def test_moliverse():
+    r = requests.get(f"{PRODUCTION_URL}/embedding_json_classic_global")
+    r.raise_for_status()
+
+    r = requests.get(f"{PRODUCTION_URL}/embedding_data_classic_global")
+    r.raise_for_status()
+
+    r = requests.get(f"{PRODUCTION_URL}/embedding_metadata_classic_global")
+    r.raise_for_status()
+
+def test_entry():
+    url = f"{PRODUCTION_URL}/analyzeentryclassic"
+    PARAMS = {'peaks': open("Data/CDCl3_SwinholideA.csv").read()} 
+    requests.post(url, params=PARAMS)
+
+def test_api():
+    df = pd.read_csv("Data/CDCl3_SwinholideA.csv", sep=",")
+    peaks_json = df.to_dict(orient="records")
+    r = requests.post(f"{PRODUCTION_URL}/api/classic/embed", data={"peaks":json.dumps(peaks_json)})
+    r.raise_for_status()
--- test/test_images.py
+++ test/test_images.py
@@ -2,6 +2,5 @@ import sys
 sys.path.insert(0, "..")
 import smart_utils
 
-
 def test_draw():
-    smart_utils.draw_nmr("CDCl3_SwinholideA.csv", "CDCl3_SwinholideA.png")
+    smart_utils.draw_nmr("Data/CDCl3_SwinholideA.csv", "CDCl3_SwinholideA.png")
