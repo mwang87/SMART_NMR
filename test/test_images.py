895f6cc446d85db279c24e638d0a7efa9f3891ce
Mingxun Wang <mwang87@gmail.com>
2020-02-20 13:59:51 -0800

    adding parsing

--- test/test_images.py
+++ test/test_images.py
@@ -4,3 +4,6 @@ import smart_utils
 
 def test_draw():
     smart_utils.draw_nmr("Data/CDCl3_SwinholideA.csv", "CDCl3_SwinholideA.png")
+    smart_utils.draw_nmr("Data/cyclomarin_A_duggan2_input.csv", "x.png")
+    smart_utils.draw_nmr("Data/cyclomarin_A_duggan_tsv.txt", "x.png")
+    smart_utils.draw_nmr("Data/cyclomarin_A_fenical_tsv.txt", "x.png")
