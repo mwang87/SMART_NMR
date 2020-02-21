e2bfa5be1d2144a973e5adb07333320aabdc079f
Mingxun Wang <mwang87@gmail.com>
2020-02-21 13:25:37 -0800

    adding testing to pull all data from test folder

--- test-integration/test-integration.py
+++ test-integration/test-integration.py
@@ -2,6 +2,7 @@ import requests
 import os
 import pandas as pd
 import json
+import glob
 
 PRODUCTION_URL = os.environ.get("SERVER_URL", "https://smart.ucsd.edu")
 
@@ -21,47 +22,34 @@ def test_moliverse():
 
 def test_entry():
     url = f"{PRODUCTION_URL}/analyzeentryclassic"
-    PARAMS = {'peaks': open("Data/CDCl3_SwinholideA.csv").read()} 
-    requests.post(url, params=PARAMS)
 
-    PARAMS = {'peaks': open("Data/cyclomarin_A_duggan2_input.csv").read()} 
-    requests.post(url, params=PARAMS)
+    test_files = glob.glob("Data/*")
 
-    PARAMS = {'peaks': open("Data/cyclomarin_A_duggan_tsv.txt").read()} 
-    requests.post(url, params=PARAMS)
-
-    PARAMS = {'peaks': open("Data/cyclomarin_A_fenical_semicolon.csv").read()} 
-    requests.post(url, params=PARAMS)
-
-    PARAMS = {'peaks': open("Data/cyclomarin_A_fenical_tsv.txt").read()} 
-    requests.post(url, params=PARAMS)
-
-    PARAMS = {'peaks': open("Data/topspin_peak.txt").read()} 
-    requests.post(url, params=PARAMS)
+    for test_file in test_files:
+        print(test_file)
+        PARAMS = {'peaks': open(test_file).read()} 
+        r = requests.post(url, params=PARAMS)
+        r.raise_for_status()
 
 def test_upload():
     url = f"{PRODUCTION_URL}/analyzeuploadclassic"
-    files = {'file': open("Data/CDCl3_SwinholideA.csv").read()}
-    r = requests.post(url, files=files)
 
-    files = {'file': open("Data/cyclomarin_A_duggan2_input.csv").read()}
-    r = requests.post(url, files=files)
+    test_files = glob.glob("Data/*")
 
-    files = {'file': open("Data/cyclomarin_A_duggan_tsv.txt").read()}
-    r = requests.post(url, files=files)
+    for test_file in test_files:
+        print(test_file)
+        files = {'file': open(test_file).read()}
+        r = requests.post(url, files=files)
+        r.raise_for_status()
 
-    files = {'file': open("Data/cyclomarin_A_fenical_semicolon.csv").read()}
-    r = requests.post(url, files=files)
-
-    files = {'file': open("Data/cyclomarin_A_fenical_tsv.txt").read()}
-    r = requests.post(url, files=files)
-
-    files = {'file': open("Data/topspin_peak.txt").read()}
-    r = requests.post(url, files=files)
 
 def test_api():
-    df = pd.read_csv("Data/CDCl3_SwinholideA.csv", sep=",")
-    peaks_json = df.to_dict(orient="records")
-    r = requests.post(f"{PRODUCTION_URL}/api/classic/embed", data={"peaks":json.dumps(peaks_json)})
-    r.raise_for_status()
+    test_files = glob.glob("Data/*")
+
+    for test_file in test_files:
+        print(test_file)
+        df = pd.read_csv(test_file, sep=",")
+        peaks_json = df.to_dict(orient="records")
+        r = requests.post(f"{PRODUCTION_URL}/api/classic/embed", data={"peaks":json.dumps(peaks_json)})
+        r.raise_for_status()
 
