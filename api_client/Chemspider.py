0a58b5665caf93d2937c39a29b1a96e31885181b
ChungmaruQ <51690359+ChungmaruQ@users.noreply.github.com>
2020-03-02 09:28:45 -0800

    Create Chemspider.py

---
+++ api_client/Chemspider.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import time
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+
+def Find_ChemSP(Name): #Compound Name
+    Name = Name.replace(' ','%20',5) #removing space from compound name
+    delay = np.random.randint(1,4) # for avoiding blocking 
+    time.sleep(delay)
+    url = f"https://www.chemspider.com/Search.aspx?q={Name}" #seraching URL
+    result = urlopen(url)
+    html = result.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    smiles = soup.select('span[id=ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_moreDetails_WrapControl2]')
+    
+    return smiles
