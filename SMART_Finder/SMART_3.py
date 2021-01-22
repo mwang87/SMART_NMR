a3bba0a999336b227973a4482b3e0ba50356254f
ChungmaruQ <51690359+ChungmaruQ@users.noreply.github.com>
2021-01-22 14:41:18 -0800

    Update SMART_3.py

--- SMART_Finder/SMART_3.py
+++ SMART_Finder/SMART_3.py
@@ -12,7 +12,7 @@ from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import datetime
 import os
 import sys
@@ -32,23 +32,23 @@ import time
 import json
 
 import argparse
-scale = 128 
+scale = 128 #scale of the input data. This scale will not be changed.
 
 def load_models(models_folder="models"):
     #importing trained model
     #If no gpus are available, these models are working with CPU automatically
+    #each channel has two models
     with tf.device('/CPU:0'):
-        model_1ch = keras.models.load_model(os.path.join(models_folder, '(011721)SMART3_v3_1ch_RC.hdf5'))
-        model_2ch = keras.models.load_model(os.path.join(models_folder, '(011721)SMART3_v3_2ch_RC.hdf5'))
-        model_1ch_class = keras.models.load_model(os.path.join(models_folder, '(011621)SMART3_v3_1ch_class_g.hdf5'))
-        model_2ch_class = keras.models.load_model(os.path.join(models_folder, '(011621)SMART3_v3_2ch_class_g.hdf5'))
-        #model_mw = keras.models.load_model(os.path.join(models_folder, 'VGG16_high_aug_MW_continue.hdf5'))
-
+        model_1ch = keras.models.load_model(os.path.join(models_folder, '(011721)SMART3_v3_1ch_RC.hdf5')) #To predict chemical fingerprints and molecular weights
+        model_1ch_class = keras.models.load_model(os.path.join(models_folder, '(011621)SMART3_v3_1ch_class_g.hdf5')) #to predict chemical class 
+        model_2ch = keras.models.load_model(os.path.join(models_folder, '(011721)SMART3_v3_2ch_RC.hdf5')) #To predict chemical fingerprints and molecular weights
+        model_2ch_class = keras.models.load_model(os.path.join(models_folder, '(011621)SMART3_v3_2ch_class_g.hdf5')) #to predict chemical class 
+        
     return model_1ch, model_2ch, model_1ch_class, model_2ch_class 
 
 def load_db(db_folder="."):
     #loading DB
-    #DB = np.load(os.path.join(db_folder, 'FPinder_DB.npy'), allow_pickle=True)
+    
     DB =np.array(pd.read_json('DB_010621_SM3.json'))
     with open('superclass.json','r') as r:
         index_super = json.load(r)
@@ -61,78 +61,86 @@ def cosine(x,y):
     b = set(y)
     return (len(a&b))/(sqrt(len(a))*sqrt(len(b)))
 
-
-def CSV_converter(CSV_converter, channel): # Converting CSV file to numpy array (128 x 128), # i = CSV file name
-    _, ext = os.path.splitext(CSV_converter)
-    if ext in [".xls",".xlsx"]:
+# This function is for converting CSV, TSV, or xlsx file to numpy array (128 x 128 x 1) or (128 x 128 x 2)
+def CSV_converter(CSV_converter, channel): 
+    _, ext = os.path.splitext(CSV_converter) # extracting filename extension to open the file.
+    if ext in [".xls",".xlsx"]: #for Excel
         qc = pd.read_excel(CSV_converter)
-    elif ext == '.csv':
+    elif ext == '.csv': #for CSV
         qc = pd.read_csv(CSV_converter)
-    elif ext == '.tsv':
+    elif ext == '.tsv': #for TSV
         qc = pd.read_csv(CSV_converter, sep="\t")
     
-    qc = qc.dropna()
-    H = (qc['1H']*scale//12).astype(int)
-    C = (qc['13C']*scale//240).astype(int)
-    if channel == 2: # edited HSQC with intensity
-        T = qc['Intensity']
+    qc = qc.dropna() #removing nan, none from the data
+    H = (qc['1H']*scale//12).astype(int) #scaling for proton, 0~12 ppm
+    C = (qc['13C']*scale//240).astype(int) #scaling for carbon, 0~240 ppm
+    if channel == 2: # When user chooses 'edited hsqc data' as input. the edited hsqc data should have three colums. '13C', '1H', and 'Intensitiy'
+        T = qc['Intensity'] # To call intensity data
+        #matrix for input
         mat = np.zeros((scale,scale,2), float)
         for j in range(len(qc)): 
             a, b = H.iloc[j].astype(int), C.iloc[j].astype(int)
             t = T.iloc[j]
             if 0 <= a < scale and 0 <= b < scale and t > 0:# + phase
-                mat[b, scale-a-1,0] = 1
+                mat[b, scale-a-1,0] = 1 
             elif 0 <= a < scale and 0 <= b < scale and t < 0:# - phase
                 mat[b, scale-a-1,1] = 1
-    elif channel == 1:
+    elif channel == 1: #When user chooses 'normal HSQC data' as input. 'Intensity' column isn't essential.
         mat = np.zeros((scale,scale,1), float)
         for j in range(len(qc)): 
             a, b = H.iloc[j].astype(int), C.iloc[j].astype(int)
             if 0 <= a < scale and 0 <= b < scale:
                 mat[b, scale-a-1,0] = 1
-    return mat
+    return mat #it should be 128x128x1 for normal HSQC data, and 128x128x2 for edited HSQC data
 
-def predict_nmr(input_nmr_filename, channel, model, model_class):
-    mat = CSV_converter(input_nmr_filename, channel)
-
+#prediction function with two models
+def predict_nmr(input_nmr_filename, channel, model, model_class): #filenamae, channel(normal/edited HSQC, 1/2)
+    #To predict chemical fingerprints, molecular weights, compound class with probability, and glycoside checking with probability
+    
+    mat = CSV_converter(input_nmr_filename, channel) #output shape should be (128,128,1) or (128,128,2)
     ### Model Prediction
+    ## Fingerprint and molecular weight prediction
     fingerprint_prediction, pred_MW  = model.predict(mat.reshape(1,scale,scale,channel))
-    fingerprint_prediction = fingerprint_prediction[0].round()
-    pred_MW = pred_MW[0][0]
-    #TODO: Annotate Logic Here
-    fingerprint_prediction = np.where(fingerprint_prediction == 1)[0]
+    fingerprint_prediction = fingerprint_prediction[0].round() 
+    fingerprint_prediction = np.where(fingerprint_prediction == 1)[0] #list of fingerprint
+    pred_MW = pred_MW[0][0] # molecular weight
+    
+    
+    ##Class prediction
     pred_c = model_class.predict(mat.reshape(1,scale,scale,channel))
-    pred_gly = pred_c[1][0][1]
     pred_class = pred_c[0][0]
     pred_class_index= sorted(range(len(pred_class)),key= lambda i: pred_class[i])[-3:] #top 5
     pred_class_index.reverse()
     pred_class_prob = pred_class[pred_class_index]
     
-    return fingerprint_prediction, pred_MW, pred_class_index, pred_class_prob, pred_gly #array, array, array
+    ##Glycoside prediction
+    pred_gly = pred_c[1][0][1]
+    
+    #Return prediction results. Fingerprints, molecular weights, Compound class index, Compound class probability, and Glycoside probability
+    return fingerprint_prediction, pred_MW, pred_class_index, pred_class_prob, pred_gly
 
 def search_database(fingerprint_prediction, pred_MW, DB, mw=None, top_candidates=20):
-    #TODO: Annotate Logic Here
+    # To compare chemical fingerprints and molecular weights to find the molecules having similar properties (FP, MW)
     # Database structure, 2 must be the predictions for the DB, 3, must be the mass
-    #topK = np.full((len(DB),4), np.nan, dtype=object)
-    results_list = []
+    
     DB_len = len(DB)
     topK = np.full((DB_len,4), np.nan, dtype=object)
     for j in range(DB_len):
-        if mw == None: #If we don't provide a user input mw, we should use the prediction
+        if mw == None: #If we don't provide a user input mw, we should use the predicted MW
             DB_mw = DB[j][3]
-            if abs(DB_mw-pred_MW)/(DB_mw) < 0.2 :
+            if abs(DB_mw-pred_MW)/(DB_mw) < 0.2 : #threshold is +- 20%
                 DB_fp = DB[j][1]
                 score = cosine(fingerprint_prediction,DB_fp)
                 if score > 0.6:
-                    topK[j] = DB[j][0], DB[j][2], score, DB_mw
+                    topK[j] = DB[j][0], DB[j][2], score, DB_mw #Compound Name (list), SMILES (str), score, molecular weight of DB compounds 
         
-        else:
+        else: # If we provide a user input mw
             real_mw = DB[j][3]
             if abs(real_mw-pred_mw) <20:
                 DB_fp = DB[j][1]
                 score = cosine(fingerprint_prediction,DB_fp)
                 if score > 0.6:
-                    topK[j] = DB[j][0], DB[j][2], score, DB_mw
+                    topK[j] = DB[j][0], DB[j][2], score, DB_mw #Compound Name (list), SMILES (str), score, molecular weight of DB compounds 
             
 
     
@@ -141,15 +149,15 @@ def search_database(fingerprint_prediction, pred_MW, DB, mw=None, top_candidates
     topK = topK.dropna(how='all')
     topK = topK.sort_values(['Cosine score'], ascending = False)
     topK = topK.drop_duplicates(['SMILES'])
-    topK = topK[:top_candidates]
+    topK = topK[:top_candidates] #remaining topN candidates
     topK = topK.fillna('No_name') #Time
 
     return topK
 
 def search_CSV(input_nmr_filename, DB, model, model_class, channel, output_table, output_nmr_image, output_pred_fingerprint, mw=None, top_candidates=20): # i = CSV file name
     mat = CSV_converter(input_nmr_filename,channel)
-
-    # plotting and saving constructed HSQC images
+    # Searching the molecules and drawing the HSQC spectra image with classificaiton results.
+    # plotting and saving constructed HSQC images with predicted class
     ## image without padding and margin
 
 
@@ -163,8 +171,8 @@ def search_CSV(input_nmr_filename, DB, model, model_class, channel, output_table
     #Saving the predicted Fingerprint
     open(output_pred_fingerprint, "w").write(json.dumps(fingerprint_prediction.tolist()))
 
-    #TODO: Evaluate if we want to remove the code to not have the drawing
-    #draw_candidates(topK, output_candidate_image)
+    
+    #Drawing constructed HSQC spectra
     height, width = scale, scale
     plt.figure()
     ax = plt.axes()
@@ -191,9 +199,6 @@ def search_CSV(input_nmr_filename, DB, model, model_class, channel, output_table
     plt.savefig(output_nmr_image, dpi=600)
     plt.close()
 
-
-    #TODO: Evaluate if we want to remove the code to not have the drawing
-    #draw_candidates(topK, output_candidate_image)
 
 def main():
     DB, index_super = load_db()
